14208b71a8ea97950502403084c251d48158fa7f
skahn <d1215@naver.com>
2025-02-11 20:53:25 +0900

    feat: first commit

---
+++ src/main/java/com/p8labs/config/ConfigApplication.java
@@ -0,0 +1,13 @@
+package com.p8labs.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p8labs/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p8labs.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
